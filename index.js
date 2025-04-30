c4cf410d190750760f4681df70e99302450c737f
Marlom Figueiredo Souza <marlom1012@gmail.com>
2025-04-30 15:34:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import express from "express"
 const app = express()
 const port = 3000
 
+// Health check endpoint
+app.get("/", (req, res) => {
+  res.status(200).send("OK")
+})
+
+
 app.get("/time", (req, res) => {
   const currentTime = new Date().toISOString()
   res.json({ time: currentTime })
