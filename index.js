6a47674f7e8c1261848c2cf25faf5ba85c923fa6
Marlom Figueiredo Souza <marlom1012@gmail.com>
2025-06-06 13:47:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ app.get("/", (req, res) => {
   res.status(200).send("OK")
 })
 
-
+// Time endpoint
 app.get("/time", (req, res) => {
   const currentTime = new Date().toISOString()
   res.json({ time: currentTime })
+})
+
+// Envs endpoint
+app.get("/envs", (req, res) => {
+  res.json(process.env)
 })
 
 app.listen(port, () => {
